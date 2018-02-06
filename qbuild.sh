1bc5e32dd12ae6690268ea60404fd5d7df943dc5
Roy Prins <rajprins@gmail.com>
2018-02-06 14:49:38 +0100

    Fixed potential resource path issue

---
+++ qbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clean up old targets, if any
+echo "- cleaning up old targets"
+rm -rf macos 2> /dev/null
+rm nettest-macos.tar.gz 2> /dev/null
+
+# Compile for target platform
+echo "- compiling and building"
+GOOS=darwin GOARCH=amd64 go build -o macos/nettest *.go
+
+# Wrap up
+cp config.yaml macos/
+echo
+echo "All done."
+
